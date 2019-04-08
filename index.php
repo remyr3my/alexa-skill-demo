0714efdb0194e251ca2e24c64b67b44f01556a4f
Ramesh R <remyr3my@gmail.com>
2019-04-08 17:40:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,8 @@
  $text = "hello Alexa, Remy ";
 
 if (isset($_POST['username'])) {
- 
     $text = "hello Alexa, Remy here with  " . $_POST['username'] ;
 }
-
 
 $array = array("version" => "1.0", "response" => array("outputSpeech" => array("type" => "PlainText", "text" => $text)));
 
